34ecaf64ba843bd8ab1ae32136f5d8b41bf05b6c
DmitriiBogomolov <foreo1997@gmail.com>
2020-04-05 00:14:45 +1000

    first commit

---
+++ posts/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Post, Group
+from .forms import PostForm
+import datetime as dt
+
+
+def index(request):
+    latest = Post.objects.order_by("-pub_date")[:11]
+    return render(request, "index.html", {"posts": latest})
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    latest = Post.objects.filter(group=group).order_by("-pub_date")[:12]
+    return render(request, "group.html", {"group": group, "posts": latest})
+
+
+@login_required
+def new_post(request):
+    date = dt.datetime.today()
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect(reverse('index'))
+
+        return render(request, 'new_post.html', {'form': form, 'date': date})
+
+    form = PostForm()
+    return render(request, 'new_post.html', {'form': form, 'date': date})
